93c4b652be64bf125f63f43566433c04ccbeecc6
杨恺10195418 <10195418@zte.intra>
2016-09-24 20:21:49 +0800

    demo

--- src/app/main/full.module.ts
+++ src/app/main/full.module.ts
@@ -3,11 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
-import { MdButtonModule } from '@angular2-material/button';
-import { MdSidenavModule} from '@angular2-material/sidenav';
-import { MdToolbarModule} from '@angular2-material/toolbar';
-import { MdCardModule} from '@angular2-material/card';
-
 import { FullComponent } from './full.component';
 import { fullRouting } from './full.routing';
 
@@ -20,10 +15,6 @@ import { fullRouting } from './full.routing';
         BrowserModule,
         FormsModule,
         HttpModule,
-        MdButtonModule.forRoot(),
-        MdSidenavModule.forRoot(),
-        MdToolbarModule.forRoot(),
-        MdCardModule.forRoot(),
         fullRouting
     ],
     providers: []
